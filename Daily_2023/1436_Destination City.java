8b77a5814693d855bac026e4c406df68924a6216
danny boi <43886538+danushv@users.noreply.github.com>
2023-12-15 11:22:05 -0600

    tried video solution

---
+++ Daily_2023/1436_Destination City.java
@@ -0,0 +1,25 @@
+#You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi.
+# Return the destination city, that is, the city without any path outgoing to another city.
+
+#It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+
+
+
+
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> start=new HashSet<>();
+
+        for(List<String> path:paths)
+        {
+            start.add(path.get(0));
+        }
+        for(List<String> path:paths)
+        {
+            String dest=path.get(1);
+            if(!start.contains(dest))
+            return dest;
+        }
+        return "";
+    }
+}
