07a4f0fa18b008d5baecd8873fa1bf82a5cebaca
danny boi <43886538+danushv@users.noreply.github.com>
2024-03-21 10:08:28 -0500

    easy

---
+++ March2024/ReverseLinkedList.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        // ListNode prev=null;
+        // ListNode curr=head;
+        // while(curr!=null)
+        // {
+        //     ListNode next=curr.next;
+        //     curr.next=prev;
+        //     prev=curr;
+        //     curr=next;
+
+
+        // }
+        // return prev;
+
+        if(head==null||head.next==null)
+        return head;
+
+        ListNode p=reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        return p;
+
+    }
+}
